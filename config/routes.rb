3e774fe6dccb9aa927434a06409f0ea53e8e241c
Stefan Lance <stefan@lances.net>
2014-06-19 17:23:53 -0400

    Fix signout bug

--- config/routes.rb
+++ config/routes.rb
@@ -8,16 +8,16 @@ Unote::Application.routes.draw do
   # Root site path (directs to static_pages's home action)
   root 'static_pages#home'
 
+  # Sessions routes
+  match '/signup', to: 'users#new', via: 'get'
+  match '/signin', to: 'sessions#new', via: 'get'
+  match '/signout', to: 'sessions#destroy', via: 'delete'
+
   # Static pages routes
   match '/signup', to: 'users#new', via: 'get'
   match '/help', to: 'static_pages#help', via: 'get'
   match '/about', to: 'static_pages#about', via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
-
-  # Sessions routes
-  match '/signup', to: 'users#new', via: 'get'
-  match '/signin', to: 'sessions#new', via: 'get'
-  match '/signout', to: 'sessions#destroy', via: 'delete'
 
 
   # The priority is based upon order of creation: first created -> highest priority.
